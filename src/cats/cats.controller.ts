1e7e31e731dbf0c363a7ecda5811c7c389ecf4ff
e1fang <438283787@qq.com>
2023-06-29 16:30:56 +0800

    feat: response

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -6,10 +6,16 @@ import {
   Patch,
   Param,
   Delete,
+  Req,
+  HttpCode,
+  Header,
+  Res,
+  HttpStatus,
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { Request, Response } from 'express';
 
 @Controller('cats')
 export class CatsController {
@@ -21,15 +27,26 @@ export class CatsController {
   }
 
   @Get()
-  findAll() {
-    return this.catsService.findAll();
+  @HttpCode(204)
+  @Header('Cache-Control', 'none')
+  findAll(@Req() req: Request, @Res() res: Response) {
+    // const reqs = req.headers.host;
+    res.status(HttpStatus.OK).json({
+      code: 200,
+      success: true,
+      data: {
+        res: this.catsService.findAll(),
+      },
+    });
+    // return reqs + this.catsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string, @Param('name') name: string) {
+  // 在路由参数传过来的都是string
+  findOne(@Param('id') id: string) {
     console.log(id);
-    const res = id + name;
-    return this.catsService.findOne(+res);
+    console.log(typeof id);
+    return this.catsService.findOne(+id);
   }
   @Get('/users/:username/posts/:postId')
   getSth(@Param('username') username: string, @Param('postId') postId: string) {
